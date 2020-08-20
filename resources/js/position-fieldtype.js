71d3015c1e7716d9388605726cb7fecfe75e10f0
Rias <hey@rias.be>
2020-08-20 10:06:33 +0200

    Update build system

--- resources/js/position-fieldtype.js
+++ resources/js/position-fieldtype.js
@@ -1,4 +1,7 @@
+import PositionFieldtype from "./PositionFieldtype";
+import PositionFieldtypeIndex from "./PositionFieldtypeIndex";
+
 Statamic.booting(() => {
-    Statamic.component('position-fieldtype', require('./PositionFieldtype.vue'));
-    Statamic.component('position-fieldtype-index', require('./PositionFieldtypeIndex.vue'));
+    Statamic.component('position-fieldtype', PositionFieldtype);
+    Statamic.component('position-fieldtype-index', PositionFieldtypeIndex);
 });
